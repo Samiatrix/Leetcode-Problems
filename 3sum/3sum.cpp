dc2527c4abc487934e02fd6277c79b96100f127d
Haridas <37403386+Samiatrix@users.noreply.github.com>
2021-12-26 18:34:30 +0530

    Time: 72 ms (71.68%), Space: 20 MB (85.19%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        set<vector<int>> s;
+        // set<vector<int>> s;
         sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
             int t = 0-nums[i];
             int l = i+1, r = nums.size()-1;
             while(l<r){
-                if(nums[l]+nums[r] <= t){
-                    if(nums[l]+nums[r] == t)    s.insert({nums[i], nums[l], nums[r]});
-                    // break;
+                if(nums[l]+nums[r] == t){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while(l<r && nums[l] == nums[l+1])  l++;
+                    while(l<r && nums[r] == nums[r-1])  r--;
+                    r--;
                     l++;
                 }
+                else if(nums[l]+nums[r] < t)   l++;
                 else{
                     r--;
                 }
             }
         }
-        for(auto i:s)   ans.push_back(i);
         return ans;
         
     }
