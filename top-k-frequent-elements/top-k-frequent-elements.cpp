ad23c0123336130eb268bd17be4052269c3c3852
Haridas <37403386+Samiatrix@users.noreply.github.com>
2021-12-29 01:55:12 +0530

    Time: 16 ms (67.29%), Space: 15.5 MB (5.58%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,19 +1,34 @@
+struct compare{
+    bool operator()(pair<int, int>& a, pair<int, int>& b){
+        return a.first>b.first;
+    }
+};
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
-        for(auto i:nums){
-            mp[i]++;
-        }
-        priority_queue<pair<int, int>> pq;
-        vector<int> ans;
+        for(auto i:nums)    mp[i]++;
+        vector<vector<int>> bucket(nums.size()+1);
         for(auto i:mp){
-            pq.push({i.second, i.first});
-            if(pq.size()>(mp.size()-k)){
-                ans.push_back(pq.top().second);
-                pq.pop();
+            bucket[i.second].push_back(i.first);
+        }
+        
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+//         for(auto i:mp){
+//             pq.push({i.second, i.first});
+//             if(pq.size()>k) pq.pop();
+//         }
+        vector<int> ans;
+        for(int i=bucket.size()-1;i>=0;i--){
+            for(int j=0;j<bucket[i].size();j++){
+                ans.push_back(bucket[i][j]);
+                if(ans.size() == k) return ans;
             }
         }
+        // while(!pq.empty()){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
         return ans;
     }
 };
