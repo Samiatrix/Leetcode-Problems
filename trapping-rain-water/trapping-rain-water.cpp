3dd98b1d2f31e2b910a959f7e1b7fcbc193db1a8
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-02 20:36:49 +0530

    Time: 12 ms (42.38%), Space: 15.5 MB (97.28%) - LeetHub

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int pre = 0;
-        // vector<int> rain(INT_MAX);
-        vector<int> left(height.size());
-        for(int i=0;i<height.size();i++){
-            if(pre<height[i]){
-                pre = height[i];
-            }
-            left[i] = pre;
-        }
-        vector<int> right(height.size());
-        pre = 0;
-        for(int i=height.size()-1;i>=0;i--){
-            if(pre<height[i]){
-                pre = height[i];
-            }
-            right[i] = pre;
-        }
+        // int pre = 0;
+        // vector<int> left(height.size());
+        // for(int i=0;i<height.size();i++){
+        //     if(pre<height[i]){
+        //         pre = height[i];
+        //     }
+        //     left[i] = pre;
+        // }
+        // vector<int> right(height.size());
+        // pre = 0;
+        // for(int i=height.size()-1;i>=0;i--){
+        //     if(pre<height[i]){
+        //         pre = height[i];
+        //     }
+        //     right[i] = pre;
+        // }
         int ans = 0;
-        for(int i=0;i<height.size();i++){
-            ans += (min(left[i], right[i]))-height[i];
+        // for(int i=0;i<height.size();i++){
+        //     ans += (min(left[i], right[i]))-height[i];
+        // }
+        int l = 0, r = height.size()-1;
+        int left = 0, right = 0;
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>=left) left = height[l];
+                else{
+                    ans+=(left-height[l]);
+                }
+                l++;
+            }
+            else{
+                if(height[r]>=right)    right = height[r];
+                else    ans+=(right-height[r]);
+                r--;
+            }
         }
-        // for(auto i:height)  cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i:left)    cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i:right)    cout<<i<<" ";
-        // cout<<endl;
         return ans;
     }
 };
