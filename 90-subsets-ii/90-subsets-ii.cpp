baa04305f3d37ed9c6a2f4fc8a2812ba2cb12354
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-05 00:39:10 +0530

    Time: 8 ms (27.19%), Space: 7.8 MB (47.49%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -3,7 +3,7 @@ public:
     void subset(vector<int>& nums, int ind, vector<int> curr, vector<vector<int>>& ans){
         ans.push_back(curr);
         for(int i=ind;i<nums.size();i++){
-            if(i == ind || nums[i]!=nums[i-1]){
+            if(ind == i || nums[i]!=nums[i-1]){
                 curr.push_back(nums[i]);
                 subset(nums, i+1, curr, ans);
                 curr.pop_back();
