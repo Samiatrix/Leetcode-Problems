6b26ea57fe4d48a75418a7047436f083a57c6d31
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-09 12:24:00 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void insert(int t, stack<int>& s){
+    if(s.empty()){
+        s.push(t);
+        return;
+    }
+    else if(!s.empty() && s.top()>t){
+        int tt = s.top();
+        s.pop();
+        insert(t, s);
+        s.push(tt);
+        return;
+    }
+    else if(!s.empty() && s.top()<=t){
+        s.push(t);
+    }
+}
+void sorted(stack<int>& s){
+    if(s.empty()){
+        return;
+    }
+    int t = s.top();
+    s.pop();
+    sorted(s);
+    insert(t, s);
+}
+void SortedStack :: sort()
+{
+   sorted(s);
+}
