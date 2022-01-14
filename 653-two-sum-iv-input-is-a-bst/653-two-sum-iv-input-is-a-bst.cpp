5c37779574dcf13a97cf13cab454a6c3898e863f
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-15 03:18:56 +0530

    Time: 59 ms (28.67%), Space: 35.9 MB (99.52%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,22 +11,13 @@
  */
 class Solution {
 public:
-    vector<int> in;
-    void inorder(TreeNode* root){
-        if(!root)   return;
-        inorder(root->left);
-        in.push_back(root->val);
-        inorder(root->right);
-    }
     bool find(TreeNode* root, TreeNode* curr, int t){
         if(root == NULL)    return false;
-        
-        return (root->val == t && root != curr) || find(root->left, curr, t) || find(root->right, curr, t);
+        return (root->val == t && root!=curr) || find(root->left, curr, t) || find(root->right, curr, t);
     }
     bool dfs(TreeNode* root, TreeNode* curr, int k){
-        if(!curr)   return false;
-        
-        return find(root, curr, k-(curr->val)) || dfs(root, curr->left, k) || dfs(root, curr->right, k);
+        if(curr == NULL)    return false;
+        return find(root, curr, k-curr->val) || dfs(root, curr->left, k) || dfs(root, curr->right, k);
     }
     bool findTarget(TreeNode* root, int k) {
         return dfs(root, root, k);
