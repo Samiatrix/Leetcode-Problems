7650824648ee91e07cb5563207507514cc37a841
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-16 03:28:08 +0530

    Time: 1136 ms (20.10%), Space: 7.3 MB (36.04%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.find(needle)==string::npos) return -1;
-        size_t i = haystack.find(needle);
-        return int(i);
+        if(needle.size() == 0)  return 0;
+        if(haystack.size() == 0)    return -1;
+        for(int i=0;i<haystack.size();i++){
+            if(i+needle.size()>haystack.size())    break;
+            for(int j=0, k=i;j<needle.size();j++){
+                if(haystack[k+j] != needle[j])    break;
+                if(j == needle.size()-1)  return i;
+            }
+        }
+        return -1;
     }
 };
