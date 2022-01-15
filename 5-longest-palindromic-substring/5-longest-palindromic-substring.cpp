65696b8912a31cd1f6be29910c026c9ed01224aa
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-16 01:18:07 +0530

    Time: 253 ms (35.68%), Space: 231.5 MB (22.49%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    string palindrome(string s, int l, int r){
-        if(s[l]!=s[r])  return "";
+    pair<int, int> palindrome(string s, int l, int r){
+        if(s[l]!=s[r])  return {0, 0};
         while(l>=0 && r<s.size() && s[l] == s[r]){
-            // cout<<l<<" "<<r<<endl;
             l--;
             r++;
         }
-        // cout<<"final"<<l<<" "<<r<<endl;
-        return s.substr(l+1, r-l-1);
+        return {l+1, r-l-1};
+        // return s.substr(l+1, r-l-1);
     }
     string longestPalindrome(string s) {
-        string ans = "";
-        int g = 0;
+        int l = 0;
+        int len = 0;
         for(int i=0;i<s.size();i++){
-            string a = palindrome(s, i, i);
-            // cout<<a<<endl;
-            string b = palindrome(s, i, i+1);
-            // cout<<a<<" "<<b<<endl;
-            if(a.size()>g){
-                ans = a;
-                g = a.size();
+            auto a = palindrome(s, i, i);
+            auto b = palindrome(s, i, i+1);
+            if(a.second>len){
+                len = a.second;
+                l = a.first;
             }
-            if(b.size()>g){
-                ans = b;
-                g = b.size();
+            if(b.second>len){
+                len = b.second;
+                l = b.first;
             }
-            // ans = max({ans, a,b});
         }
-        return ans;
+        return s.substr(l, len);
     }
 };
