9ebec3bc69f83be03c51d8a4d88386e3f297d943
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-16 07:35:43 +0530

    Time: 225 ms (39.94%), Space: 231.6 MB (20.53%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    pair<int, int> palindrome(string s, int l, int r){
-        // if(s[l]!=s[r])  return {0, 0};
-        while(l>=0 && r<s.size() && s[l] == s[r]){
+    pair<int, int> pal(string s, int l, int r){
+        if(s[l]!=s[r])	return {0, 0};
+        while(l>=0 && r<s.size() && s[l]==s[r]){
             l--;
             r++;
         }
         return {l+1, r-l-1};
     }
+
     string longestPalindrome(string s) {
-        int l = 0;
-        int len = 0;
+        int len=0, l=0;
         for(int i=0;i<s.size();i++){
-            auto a = palindrome(s, i, i);
-            auto b = palindrome(s, i, i+1);
+            pair<int, int> a = pal(s, i, i);
+            pair<int, int> b = pal(s, i, i+1);
             if(a.second>len){
                 len = a.second;
                 l = a.first;
@@ -24,5 +24,6 @@ public:
             }
         }
         return s.substr(l, len);
+
     }
 };
