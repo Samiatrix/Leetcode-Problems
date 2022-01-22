439812506fd968e3d8b61db0faee3dd307f6ee4e
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-23 01:33:22 +0530

    Time: 373 ms (16.67%), Space: 106.6 MB (66.67%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long ma = LONG_MIN;
-        long mi = LONG_MAX;
+        long ma = 0;
+        long mi = 0;
         long curr = 0;
         for(auto i:differences){
             curr+=i;
             ma = max(ma, curr);
             mi = min(mi, curr);
         }
-        int ans = 0;
-        for(int i=lower;i<=upper;i++){
-            if(ma+i<=upper && mi+i>=lower)  ans++;
-        }
+        long ans = max(0L, (upper-lower)-(ma-mi)+1);
         return ans;
         
     }
