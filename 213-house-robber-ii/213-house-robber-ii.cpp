0956f5c3339e3ded2b52e4d0dc622366f79af050
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-22 19:04:24 +0530

    Time: 3 ms (50.61%), Space: 7.9 MB (56.59%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -3,14 +3,13 @@ public:
     int adj(vector<int>& arr){
         int prev = arr[0];
         int prev2 = 0;
-        vector<int> dp(arr.size(), 0);
-        dp[0] = arr[0];
         for(int i=1;i<arr.size();i++){
-            int l = arr[i]+ (i>1 ? dp[i-2] : 0);
-            int r = dp[i-1];
-            dp[i] = max(l, r);
+            int l = arr[i]+ (i>1 ? prev2 : 0);
+            int r = prev;
+            prev2 = prev;
+            prev = max(l, r);
         }
-        return dp.back();
+        return prev;
     }
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
