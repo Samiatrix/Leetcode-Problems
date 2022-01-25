73957c8a4b46302ab5f550a25970fd77d0769def
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-01-26 04:15:47 +0530

    Time: 55 ms (13.66%), Space: 13.3 MB (93.29%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -3,22 +3,20 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n+1, -1);
-        queue<pair<int, int>> q;
+        queue<int> q;
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                q.push({i, 1});
-                
+                q.push(i);
+                color[i] = 1;
                 while(!q.empty()){
                     auto curr = q.front();
                     q.pop();
-                    color[curr.first] = 1-curr.second;
-                    // cout<<curr.first<<" "<<curr.second<<" "<<color[curr.first]<<endl;
-                    for(auto no:graph[curr.first]){
-                        // cout<<"int adj "<<no<<endl;
+                    for(auto no:graph[curr]){
                         if(color[no] == -1){
-                            q.push({no, color[curr.first]});
+                            color[no] = 1-color[curr];
+                            q.push(no);
                         }
-                        else if(color[no] == color[curr.first])   return false;
+                        else if(color[no] == color[curr])   return false;
                     }
                 }
             }
