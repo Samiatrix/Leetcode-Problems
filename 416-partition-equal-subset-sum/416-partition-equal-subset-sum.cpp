9289dbe3d5e2774a5d9f6a0e572185250003983d
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-04 01:47:34 +0530

    Time: 188 ms (40.43%), Space: 9.3 MB (73.18%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -19,21 +19,8 @@ public:
         if(nums.size() == 1)    return false;
         int sum = 0;
         for(auto i:nums)    sum+=i;
-        // if(nums.size()&1)   return false;
         if(sum&1)   return false;
         sum/=2;
         return part(nums.size()-1, sum, nums);
-        vector<vector<bool>> dp(nums.size(), vector<bool>(sum+1, false));
-        for(int i=0;i<nums.size();i++)  dp[i][0] = true;
-        dp[0][nums[0]] = true;
-        for(int i=1;i<nums.size();i++){
-            for(int j=1;j<=sum;j++){
-                bool take = (j>=nums[i] ? dp[i-1][j-nums[i]] : false);
-                bool notTake = dp[i-1][j];
-                dp[i][j] = take | notTake;
-            }
-        }
-        return dp[nums.size()-1][sum];
-        // return part(nums.size()-1, sum, nums);
     }
 };
