b4c08da37114ea1268c35f188e064b26d2376ad7
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-05 19:54:27 +0530

    Time: 29 ms (70.31%), Space: 15 MB (5.63%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int numSplits(string s) {
-        unordered_map<char, int> mp;
-        unordered_map<char, int> mpp;
-        for(auto i:s)   mp[i]++;
+        unordered_set<char> st;
+        vector<int> pre;
+        vector<int> suff(s.size());
+        for(auto i:s){
+            st.insert(i);
+            pre.push_back(st.size());
+        }
+        st.clear();
+        
+        for(int i=s.size()-1;i>=0;i--){
+            st.insert(s[i]);
+            suff[i] = st.size();
+        }
         int ans = 0;
-        int sl = 0, sr = mp.size();
+        // int sl = 0, sr = mp.size();
         for(int i=0;i<s.size()-1;i++){
-            char ch = s[i];
-            if(mp[ch] == 1){
-                mp.erase(ch);
-                mpp[ch]++;
-            }
-            else{
-                mp[ch]--;
-                mpp[ch]++;
-            }
-            ans += mp.size() == mpp.size();
+            ans += pre[i] == suff[i+1];
+            // char ch = s[i];
+            // if(mp[ch] == 1){
+            //     mp.erase(ch);
+            //     mpp[ch]++;
+            // }
+            // else{
+            //     mp[ch]--;
+            //     mpp[ch]++;
+            // }
+            // ans += mp.size() == mpp.size();
         }
         return ans;
     }
