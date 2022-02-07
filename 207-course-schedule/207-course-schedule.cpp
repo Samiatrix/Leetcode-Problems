4ae5543c604daa277d8526f730c8195be3a2a331
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-08 01:28:52 +0530

    Time: 51 ms (17.08%), Space: 14.7 MB (25.22%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
-    bool cycle(int curr, vector<bool>& vis, vector<bool>& dfs, unordered_map<int, vector<int>>& adj){
+    // bool cycle(int curr, vector<bool>& vis, vector<bool>& dfs, unordered_map<int, vector<int>>& adj){
+    //     vis[curr] = true;
+    //     dfs[curr] = true;
+    //     for(auto i:adj[curr]){
+    //         if(!vis[i]){
+    //             if(cycle(i, vis, dfs, adj)) return true;
+    //         }
+    //         else if(dfs[i]) return true;
+    //     }
+    //     dfs[curr] = false;
+    //     return false;
+    // }
+    void cycle(int curr, vector<bool>& vis, unordered_map<int, vector<int>>& adj, stack<int>& s){
         vis[curr] = true;
-        dfs[curr] = true;
         for(auto i:adj[curr]){
             if(!vis[i]){
-                if(cycle(i, vis, dfs, adj)) return true;
+                cycle(i, vis, adj, s);
             }
-            else if(dfs[i]) return true;
         }
-        dfs[curr] = false;
-        return false;
+        s.push(curr);
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int, vector<int>> adj;
         for(auto i:prerequisites){
             adj[i[1]].push_back(i[0]);
         }
-        vector<bool> vis(numCourses, false);
-        vector<bool> dfs(numCourses, false);
+        
+        vector<int> indegree(numCourses, 0);
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(cycle(i, vis, dfs, adj)) return false;
+            for(auto j:adj[i]){
+                indegree[j]++;
             }
         }
-        return true;
+        queue<int> q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int count = 0;
+        vector<bool> vis(numCourses, false);
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            count++;
+            for(auto i:adj[curr]){
+                indegree[i]--;
+                if(indegree[i] == 0)    q.push(i);
+            }
+        }
+        return numCourses == count;
     }
 };
