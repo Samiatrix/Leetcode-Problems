7feec654b7d4fc50b5c5d267621dfdbabac80475
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-08 00:59:51 +0530

    Time: 53 ms (15.13%), Space: 15.3 MB (19.02%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool cycle(int curr, vector<bool>& dfsVis, unordered_map<int, vector<int>>& adj, vector<bool>& vis){
+    bool cycle(int curr, vector<bool>& vis, vector<bool>& dfs, unordered_map<int, vector<int>>& adj){
         vis[curr] = true;
-        dfsVis[curr] = true;
-        for(auto node:adj[curr]){
-            if(!vis[node]){
-                if(cycle(node, dfsVis, adj, vis))   return true;
+        dfs[curr] = true;
+        for(auto i:adj[curr]){
+            if(!vis[i]){
+                if(cycle(i, vis, dfs, adj)) return true;
             }
-            else if(dfsVis[node])  return true;
+            else if(dfs[i]) return true;
         }
-        dfsVis[curr] = false;
+        dfs[curr] = false;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size() == 0)   return true;
         unordered_map<int, vector<int>> adj;
         for(auto i:prerequisites){
             adj[i[1]].push_back(i[0]);
         }
-        // stack<int> s;
         vector<bool> vis(numCourses, false);
-        vector<bool> dfsVis(numCourses, false);
+        vector<bool> dfs(numCourses, false);
         for(int i=0;i<numCourses;i++){
             if(!vis[i]){
-                if(cycle(i, dfsVis, adj, vis))   return false;
+                if(cycle(i, vis, dfs, adj)) return false;
             }
         }
         return true;
-        
     }
 };
