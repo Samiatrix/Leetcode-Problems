9452c345dfa051d187e419f56f0b34e75af8fe23
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-09 02:03:32 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -10,19 +10,19 @@ using namespace std;
 class Solution{
   public:
     int count(string s, int l, int r, vector<vector<int>>& dp){
-        if(l>r) return INT_MAX;
+        if(l>r) 1e8;
         
         if(l == r)  return 0;
-        
         if(l == r-1){
             return s[l] == s[r] ? 0 : 1;
         }
         if(dp[l][r]!=-1)    return dp[l][r];
-        return dp[l][r] = (s[l] == s[r] ? count(s, l+1, r-1, dp) : 1+min(count(s, l+1, r, dp), count(s, l, r-1, dp)));
+        return dp[l][r] = s[l] == s[r] ? count(s, l+1, r-1, dp) : 1+min(count(s, l+1, r, dp), count(s, l, r-1, dp));
     }
-    int countMin(string s){
-        vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
-        return count(s, 0, s.size()-1, dp);
+    int countMin(string str){
+        int n = str.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return count(str, 0, n-1, dp);
     }
 };
 
