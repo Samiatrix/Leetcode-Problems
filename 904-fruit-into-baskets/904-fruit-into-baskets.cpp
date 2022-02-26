f498694162a4545a9479b301f0c6de02c3156622
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-27 00:04:31 +0530

    Time: 180 ms (59.08%), Space: 61.4 MB (70.29%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -2,37 +2,17 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int ans = 0;
-        unordered_set<int> s;
         unordered_map<int, int> mp;
         int curr = 0;
         for(int i=0, j=0;j<fruits.size();j++){
             mp[fruits[j]]++;
             curr++;
-            cout<<curr<<endl;
             while(mp.size()>2){
-                // cout<<fruits[j]<<" <=== "<<endl;
-                // while(s.count(fruits[i]))   
                 if(mp[fruits[i]] == 1)  mp.erase(fruits[i]);
                 else    mp[fruits[i]]--;
                 curr--;
                 i++;
-                
-                // cout<<fruits[i]<<" -- "<<endl;
-                
-//                 int remf = fruits[i];
-//                 s.insert(remf);
-//                 while(s.count(fruits[i]))   i++;
-//                 cout<<fruits[i]<<" ++ "<<endl;
-                
-//                 curr-=mp[remf];
-//                 mp.erase(remf);
-//                 cout<<curr<<" ==> "<<endl;
-//                 ans = max(ans, curr);
             }
-            // else{
-            //     cout<<fruits[j]<<" "<<curr<<endl;
-            //     ans = max(ans, curr);
-            // }
             ans = max(ans, curr);
         }
         return ans;
