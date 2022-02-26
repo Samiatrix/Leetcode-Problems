74be920cfbd4debef7e37b151d364683d04a8032
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-02-26 16:18:30 +0530

    Time: 26 ms (7.69%), Space: 14 MB (50.46%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    unordered_set<string> s;
-    void dfs(vector<vector<int>>& image, int x, int y, int ocolor, int ncolor){
-        string t = to_string(x)+"#"+to_string(y);
-        if(x<0 || x>=image.size() || y<0 || y>=image[x].size() || image[x][y]!=ocolor || s.count(t))  return;
-        s.insert(t);
-        image[x][y] = ncolor;
-        dfs(image, x+1, y, ocolor, ncolor);
-        dfs(image, x-1, y, ocolor, ncolor);
-        dfs(image, x, y+1, ocolor, ncolor);
-        dfs(image, x, y-1, ocolor, ncolor);
-
+    void dfs(vector<vector<int>>& image, int x, int y, int oldColor, int newColor){
+        if(x<0 || x>=image.size() || y<0 || y>=image[x].size() || image[x][y]!=oldColor)    return;
+        image[x][y] = newColor;
+        dfs(image, x+1, y, oldColor, newColor);
+        dfs(image, x-1, y, oldColor, newColor);
+        dfs(image, x, y+1, oldColor, newColor);
+        dfs(image, x, y-1, oldColor, newColor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if(image[sr][sc] == newColor)   return image;
         dfs(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
