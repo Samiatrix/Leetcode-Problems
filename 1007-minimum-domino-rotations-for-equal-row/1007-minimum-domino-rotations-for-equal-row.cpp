56791e273d3051095cb421c4efd26c5f4cd971de
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-04 00:04:24 +0530

    Time: 188 ms (54.41%), Space: 111.6 MB (25.35%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,55 +1,18 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int top = 0;
-        unordered_map<int, int> tm;
-        int gr = -1;
-        int maxi = 0;
-        for(auto &i:tops){
-            tm[i]++;
-            if(maxi<tm[i]){
-                maxi = tm[i];
-                gr = i;
-            }
-        }
-        maxi = 0;
-        int gb = -1;
-        unordered_map<int, int> bm;
-        for(auto &i:bottoms){
-            bm[i]++;
-            if(maxi<tm[i]){
-                maxi = tm[i];
-                gb = i;
-            }
-        }
-        
-        int curr = 0;
-        int ans = INT_MAX;
-        
+        vector<int> top(7), bottom(7), same(7);
         for(int i=0;i<tops.size();i++){
-            if(gr != tops[i]){
-                if(bottoms[i] == gr)    curr++;
-                else{
-                    curr = INT_MAX;
-                    break;
-                }
+            top[tops[i]]++;
+            bottom[bottoms[i]]++;
+            if(tops[i] == bottoms[i])   same[tops[i]]++;
+        }
+        int ans = 0;
+        for(int i=0;i<7;i++){
+            if(top[i]+bottom[i]-same[i] == tops.size()){
+                return (tops.size() - max(top[i], bottom[i]));
             }
         }
-        ans = min(ans, curr);
-        curr = 0;
-        for(int i=0;i<bottoms.size();i++){
-            if(gb != bottoms[i]){
-                if(tops[i] == gb)    curr++;
-                else{
-                    curr = INT_MAX;
-                    break;
-                }
-            }
-        }
-        ans = min(ans, curr);
-        
-        return ans == INT_MAX ? -1 : ans;
-        
-        
+        return -1;
     }
 };
