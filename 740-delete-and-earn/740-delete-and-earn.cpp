ea5fd560fc1e574ff93ef621baab7f67f7b16dc1
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-06 00:59:04 +0530

    Time: 11 ms (58.71%), Space: 9.8 MB (61.62%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int maxPoints(vector<int>& nums, int curr, unordered_map<int, int>& mp, vector<int>& dp){
-        // cout<<curr<<" "<<nums[curr]<<" "<<left<<" "<<right<<endl;
         if(curr < 0)    return 0;
-        // if(curr == 0){
-        //     int res = ((nums[curr] != left && nums[curr] != right) ? (mp[nums[curr]]*nums[curr]) : -1e8);
-        //     // cout<<curr<<" "<<nums[curr]<<" "<<left<<" "<<right<<" "<<res<<endl;
-        //     // cout<<endl;
-        //     return res;
-        // }
-        // cout<<curr<<" "<<nums[curr]<<" "<<left<<" "<<right<<endl;
-        // cout<<"now Take "<<nums[curr]<<endl;
         if(dp[curr]!=-1)    return dp[curr];
         int take = (mp[nums[curr]]*nums[curr]);
         int ind = curr-1;
         while(ind>=0 && nums[ind] == nums[curr]-1)    ind--;
         return dp[curr] = max(take+maxPoints(nums, ind, mp, dp), maxPoints(nums, curr-1, mp, dp));
-        // cout<<nums[curr]<<" Take "<<take<<endl;
-        // cout<<"now NotTake "<<nums[curr]<<endl;
-        // int notTake = maxPoints(nums, curr-1, left, right, mp);
-        // cout<<nums[curr]<<" NotTake "<<take<<endl;
-        
-        // cout<<curr<<" "<<nums[curr]<<" "<<left<<" "<<right<<" "<<take<<" "<<notTake<<endl;
-        // return max(take, notTake);
     }
     int deleteAndEarn(vector<int>& nums) {
         unordered_map<int, int> mp;
@@ -31,7 +15,6 @@ public:
         for(auto i:mp)  nums.push_back(i.first);
         sort(nums.begin(), nums.end());
         vector<int> dp(nums.size(), -1);
-        // for(auto i:nums)    cout<<i<<" "<<mp[i]<<endl;
         return maxPoints(nums, nums.size()-1, mp, dp);
     }
 };
