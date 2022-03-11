e834b206133f518a62799b6bcfedc55e7e27159c
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-12 01:05:37 +0530

    Time: 30 ms (49.18%), Space: 13 MB (36.05%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -14,20 +14,20 @@ public:
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        // for(int i=0;i<m;i++){
-        //     dp[0][i] = (text1[0] == text2[i]);
-        // }
-        // for(int i=0;i<n;i++){
-        //     dp[i][0] = (text1[i] == text2[0]);
-        // }
-        // for(int i=1;i<n;i++){
-        //     for(int j=1;j<m;j++){
-        //         if(text1[i] == text2[j])    dp[i][j] = 1 + dp[i-1][j-1];
-        //         else    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        //     }
-        // }
-        // return dp[n-1][m-1];
-        return lcs(text1, text1.size()-1, text2, text2.size()-1, dp);
+        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), 0));
+        for(int i=0;i<m;i++){
+            dp[0][i] = (text1[0] == text2[i]);
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0] = (text1[i] == text2[0]);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(text1[i] == text2[j])    dp[i][j] = 1 + ((i-1>=0) && (j-1>=0) ? dp[i-1][j-1] : 0);
+                else    dp[i][j] = max((i-1>=0 ? dp[i-1][j] : 0), (j-1>=0 ? dp[i][j-1] : 0));
+            }
+        }
+        return dp[n-1][m-1];
+        // return lcs(text1, text1.size()-1, text2, text2.size()-1, dp);
     }
 };
