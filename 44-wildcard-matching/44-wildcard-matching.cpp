15b7e3891f2fd34a655bda6764ee5be5df55159a
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-16 23:09:15 +0530

    Time: 60 ms (59.24%), Space: 9.8 MB (58.65%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -18,10 +18,13 @@ public:
         
         int m = s.size(), n = p.size();
         // vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<bool> prev(m+1, false), curr(m+1, false);
         vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0] = true;
+        prev[0] = true;
+        
         
         for(int i=1;i<=n;i++){
+            
             bool flag = true;
             for(int k=1;k<=i;k++){
                 if(p[k-1]!='*'){
@@ -29,20 +32,19 @@ public:
                     break;
                 }
             }
-            dp[i][0] = flag;
-        }
-        
-        for(int i=1;i<=n;i++){
+            curr[0] = flag;
+            
             for(int j=1;j<=m;j++){
                 if(p[i-1] == s[j-1] || p[i-1] == '?'){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else if(p[i-1] == '*'){
-                    dp[i][j] = (dp[i-1][j] | dp[i][j-1]);
+                    curr[j] = (prev[j] | curr[j-1]);
                 }
-                else    dp[i][j] = false;
+                else    curr[j] = false;
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
