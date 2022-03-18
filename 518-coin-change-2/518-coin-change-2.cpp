de6d433ac62fd930ccd65f73d704622a146d5cfe
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-18 23:57:00 +0530

    Time: 13 ms (82.49%), Space: 18.4 MB (35.51%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
+    int coin(int tar, vector<int>& coins, int ind, vector<vector<int>>& dp){
+        if(ind == 0 && tar % coins[ind] == 0)   return 1;
+        else if(ind == 0 && tar%coins[ind]!=0)  return 0;
+        if(tar<0)   return 0;
+        if(dp[ind][tar]!=-1)    return dp[ind][tar];
+        int take = 0;
+        if(tar>=coins[ind]) take = coin(tar-coins[ind], coins, ind, dp);
+        int notTake = coin(tar, coins, ind-1, dp);
+        return dp[ind][tar] = take+notTake;
+    }
     int change(int amount, vector<int>& coins) {
-        if(amount == 0) return 1;
-        
-        int n = coins.size();
-        
-        if(coins.size() == 0)   return 0;
-        
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
-        for(int i=0;i<=amount;i++){
-            dp[0][i] = 0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(j>=coins[i-1]){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][amount];
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+        return coin(amount, coins, coins.size()-1, dp);
     }
 };
