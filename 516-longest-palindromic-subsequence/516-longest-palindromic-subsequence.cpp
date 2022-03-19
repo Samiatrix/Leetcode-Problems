02b3eb3aac2fda6fa67d9bc641d3bd3cd9697721
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-19 14:13:56 +0530

    Time: 144 ms (46.04%), Space: 73.2 MB (17.87%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
+    int pal(string& s, int i, string& t, int j, vector<vector<int>>& dp){
+        if(i<=0 || j<=0)  return 0;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        if(s[i-1] == t[j-1])    return dp[i][j] = 1+pal(s, i-1, t, j-1, dp);
+        else    return dp[i][j] = max(pal(s, i-1, t, j, dp), pal(s, i, t, j-1, dp));
+    }
     int longestPalindromeSubseq(string s) {
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-        int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s[i-1] == s2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][n];
+        string t = s;
+        reverse(t.begin(), t.end());
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, -1));
+        // for(int i=)
+        return pal(s, s.size(), t, t.size(), dp);
     }
 };
