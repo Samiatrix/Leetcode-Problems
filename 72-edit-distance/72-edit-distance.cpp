7f0cf6308b203747709a431611be7d0f7b157020
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-20 16:13:05 +0530

    Time: 119 ms (13.08%), Space: 82.1 MB (5.18%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    /*
-    int edit(string& s, int i, string& t, int j, vector<vector<int>>& dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+    int dis(string s, int i, string t, int j, vector<vector<int>>& dp){
+        if(i==0) return j;
+        if(j==0) return i;
+        // if(i == 0 && j == 0)    return 0;
         if(dp[i][j]!=-1)    return dp[i][j];
-        if(s[i] == t[j])    return dp[i][j] = edit(s, i-1, t, j-1, dp);
+        if(s[i-1] == t[j-1])    return dp[i][j] = dis(s, i-1, t, j-1, dp);
         else{
-            return dp[i][j] = 1+min({edit(s, i-1, t, j, dp), edit(s, i, t, j-1, dp), edit(s, i-1, t, j-1, dp)});
+            return dp[i][j] = 1+min({dis(s, i-1, t, j, dp), dis(s, i, t, j-1, dp), dis(s, i-1, t, j-1, dp)});
         }
-    }*/
+    }
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<int> prev(m+1, 0), curr(m+1, 0);
-        for(int i=1;i<=m;i++){
-            prev[i] = i;
-        }
-        for(int i=1;i<=n;i++){
-            curr[0] = i;
-            for(int j=1;j<=m;j++){
-                if(word1[i-1] == word2[j-1])    curr[j] = prev[j-1];
-                else{
-                    curr[j] = 1+min({prev[j], curr[j-1], prev[j-1]});
-                }
-            }
-            prev = curr;
-        }
-        return prev[m];
-        // return edit(word1, n-1, word2, m-1, dp);
+        if(word1 == "" || word2 == "")  return max(word1.size(), word2.size());
+        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
+        return dis(word1, word1.size(), word2, word2.size(), dp);
     }
 };
