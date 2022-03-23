e9e833e5e87ca23992ca2b41e22b4769a98244aa
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-23 23:15:43 +0530

    Time: 1804 ms (6.83%), Space: 469 MB (5.05%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,54 +1,57 @@
 class Solution {
 public:
+    
     void merge(vector<pair<int, int>>& nums, int l, int r, vector<int>& ans){
         if(l>=r)    return;
-        int mid = (l+r)/2;
+        
+        int mid = l+(r-l)/2;
         merge(nums, l, mid, ans);
         merge(nums, mid+1, r, ans);
         
         int left = l;
         int right = mid+1;
+        int numbersSmallerThanLeft = 0;
+        
         vector<pair<int, int>> merged;
-        int elementGreaterThanLeft = 0;
-        while(left<mid+1 && right<=r){
-            if(nums[left].first>nums[right].first){
-                elementGreaterThanLeft++;
-                merged.push_back(nums[right]);
-                right++;
+        
+        while(left<=mid && right<=r){
+            if(nums[left].first > nums[right].first){
+                numbersSmallerThanLeft++;
+                merged.push_back(nums[right++]);
             }
             else{
-                ans[nums[left].second]+=elementGreaterThanLeft;
-                merged.push_back(nums[left]);
-                left++;
+                ans[nums[left].second]+=numbersSmallerThanLeft;
+                merged.push_back(nums[left++]);
             }
         }
         
-        while(left<mid+1){
-            ans[nums[left].second]+=elementGreaterThanLeft;
-            merged.push_back(nums[left]);
-            left++;
+        while(left<=mid){
+            ans[nums[left].second]+=numbersSmallerThanLeft;
+            merged.push_back(nums[left++]);
         }
         
         while(right<=r){
-            // ans[nums[left].second]+=elementGreaterThanLeft;
-            merged.push_back(nums[right]);
-            right++;
+            merged.push_back(nums[right++]);
         }
-        int i = l;
-        for(auto j:merged){
-            nums[i] = j;
-            i++;
+        
+        
+        int pos = l;
+        for(auto i:merged){
+            nums[pos++] = i;
         }
         
     }
+    
     vector<int> countSmaller(vector<int>& nums) {
-        int l = 0, r = nums.size()-1;
-        vector<pair<int, int>> t;
+        vector<pair<int, int>> numi;
         for(int i=0;i<nums.size();i++){
-            t.push_back({nums[i], i});
+            numi.push_back({nums[i], i});
         }
+        
         vector<int> ans(nums.size(), 0);
-        merge(t, l, r, ans);
+        
+        merge(numi, 0, nums.size()-1, ans);
+        
         return ans;
     }
 };
