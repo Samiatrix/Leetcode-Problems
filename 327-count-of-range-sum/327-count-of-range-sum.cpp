18c20ab7a4af9cb48d415ce28d9038a96dd7a928
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-24 01:09:45 +0530

    Time: 492 ms (86.97%), Space: 194.9 MB (45.21%) - LeetHub

--- 327-count-of-range-sum/327-count-of-range-sum.cpp
+++ 327-count-of-range-sum/327-count-of-range-sum.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int count(vector<long>& sum, int lower, int upper, int l, int r){
+    int count(vector<long>& sum, int low, int upp, int l, int r){
         if(r-l<=1)  return 0;
-        
         int mid = l+(r-l)/2;
+        int ans = 0, m = mid, n = mid;
+        ans = count(sum, low, upp, l, mid) + count(sum, low, upp, mid, r);
         
-        int ans = 0;
-        int le = count(sum, lower, upper, l, mid);
-        int ri = count(sum, lower, upper, mid, r);
-        ans = le+ri;
-        
-        int m = mid, n = mid;
         for(int i=l;i<mid;i++){
-            while(m<r && sum[m]-sum[i] < lower) m++;
-            while(n<r && sum[n]-sum[i] <= upper) n++;
-            ans+=(n-m);
+            while(m<r && sum[m]-sum[i] < low)    m++;
+            while(n<r && sum[n]-sum[i] <= upp)    n++;
+            ans += n-m;
         }
         
         inplace_merge(sum.begin()+l, sum.begin()+mid, sum.begin()+r);
         return ans;
     }
     int countRangeSum(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
-        vector<long> sum(n+1, 0);
-        for(long i=0;i<n;i++)    sum[i+1] = sum[i]+nums[i];
-        return count(sum, lower, upper, 0, n+1);
+        vector<long> sum(nums.size()+1, 0);
+        for(int i=0;i<nums.size();i++)  sum[i+1] = sum[i]+nums[i];
+        return count(sum, lower, upper, 0, nums.size()+1);
     }
 };
