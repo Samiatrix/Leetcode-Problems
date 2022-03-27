d56faa2e56d5a4a32bd1f0e02653df3ae2a939b6
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-27 19:07:52 +0530

    Time: 368 ms (58.82%), Space: 54.1 MB (100.00%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -5,25 +5,6 @@ public:
         int end = (intLength%2 == 0) ? (intLength/2) : (intLength/2 + 1);
         long long ind = pow(10, start);
         long long last = pow(10, end)-1;
-        vector<string> t;
-        // int m = *max_element(queries.begin(), queries.end());
-        // for(int i=0;i<=m;i++){
-        //     string s = to_string(ind);
-        //     if(intLength & 1){
-        //         string ss = s.substr(0, s.size()-1);
-        //         reverse(ss.begin(), ss.end());
-        //         s+=(ss);
-        //         if(s.size() == intLength)   t.push_back(s);
-        //     }
-        //     else{
-        //         string ss = s;
-        //         reverse(ss.begin(), ss.end());
-        //         s+=(ss);
-        //         if(s.size() == intLength)   t.push_back(s);
-        //     }
-        //     ind++;
-        // }
-        // // sort(t.begin(), t.end());
         vector<long long> ans;
         for(int i=0;i<queries.size();i++){
             if(queries[i] <= (last-ind+1)){
@@ -46,12 +27,7 @@ public:
             else{
                 ans.push_back(-1);
             }
-            // ind++;
-            // ans.push_back(t.size()>=(queries[i]-1) ? stol(t[queries[i]-1]) : -1);
         }
-        return ans;
-        
-        
-        
+        return ans;   
     }
 };
