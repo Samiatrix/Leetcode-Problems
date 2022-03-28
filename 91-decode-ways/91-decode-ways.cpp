e71a872f1028e14a0279abb3e9753f420073be2e
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-03-29 00:49:05 +0530

    Time: 6 ms (26.19%), Space: 7.4 MB (18.62%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int decode(string s, int i, vector<int>& dp){
+    int dec(string s, int i, vector<int>& dp){
         if(i == s.size())   return 1;
-        else if(i>s.size()) return 0;
         if(s[i] == '0') return 0;
         if(dp[i]!=-1)   return dp[i];
-        int single = decode(s, i+1, dp);
-        if(i<s.size()-1 && (s[i] == '1' || (s[i]=='2' && s[i+1]<='6')))    single+=decode(s, i+2, dp);
-        return dp[i] = single;
+        int notTake = dec(s, i+1, dp);
+        int take = 0;
+        if(i+1<s.size() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')))   take = dec(s, i+2, dp);
+        return dp[i] = take+notTake;
     }
     int numDecodings(string s) {
-        vector<int> dp(s.size(), -1);
-        return decode(s, 0, dp);
+        vector<int> dp(s.size()+1, -1);
+        return dec(s, 0, dp);
     }
 };
