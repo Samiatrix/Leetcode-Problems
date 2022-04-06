7fef8c8541e83a100a1061d899497cd072c57dfc
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-04-07 00:28:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.91%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n == 0)  return 1;
-        if(n == 1)  return x;
+        // if(n == 1)  return x;
         if(n<0){
             return 1.0/x * myPow(1/x, -(n+1));
         }
