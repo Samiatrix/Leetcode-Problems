d13e75fed8f226840a2322551b6fb48b293b2bb3
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-04-07 01:56:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (14.55%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -4,10 +4,9 @@ public:
         if(i == s.size())   return 1;
         if(s[i] == '0') return 0;
         if(dp[i]!=-1)   return dp[i];
-        int notTake = dec(s, i+1, dp);
-        int take = 0;
-        if(i+1<s.size() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')))   take = dec(s, i+2, dp);
-        return dp[i] = take+notTake;
+        int ans = dec(s, i+1, dp);
+        if((i+1)<s.size() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))) ans+=dec(s, i+2, dp);
+        return dp[i] = ans;
     }
     int numDecodings(string s) {
         vector<int> dp(s.size()+1, -1);
