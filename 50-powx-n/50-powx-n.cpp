c0b809f977217ae7401763ea91149ce196181922
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-04-07 00:24:33 +0530

    Time: 2 ms (43.24%), Space: 5.9 MB (81.45%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)  return 1;
+        if(n == 1)  return x;
+        if(n<0){
+            return 1.0/x * myPow(1/x, -(n+1));
+        }
+        return n&1 ? x*myPow(x*x, n/2) : myPow(x*x, n/2);
+    }
+};
