e0105ee0a67930379174fde83b131be7c9b83a02
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-04-07 11:12:01 +0530

    Time: 29 ms (54.87%), Space: 15.6 MB (13.74%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,68 +1,39 @@
 class Solution {
 public:
-    void dfs(unordered_map<int, vector<int>>& adj, int ind, vector<bool>& vis, vector<bool>& dfsVis, stack<int>& s, bool& possible){
-        if(possible)    return;
-        // if(vis[ind])    return ;
+    bool dfs(int n, unordered_map<int, vector<int>>& mp, int ind, stack<int>& order, vector<bool>& vis, vector<bool>& dfsVis){
+        // if(vis[ind])    return;
         vis[ind] = true;
         dfsVis[ind] = true;
-        for(auto i:adj[ind]){
-            if(!vis[i]){
-                dfs(adj, i, vis, dfsVis, s, possible);
+        
+        for(auto curr:mp[ind]){
+            if(!vis[curr]){
+                // vis[curr]
+                if(dfs(n, mp, curr, order, vis, dfsVis))    return true;
             }
-            else if(dfsVis[i]){
-                possible = true;
-                return;
-            }
+            else if(dfsVis[curr])   return true;   
         }
         dfsVis[ind] = false;
-        if(!possible)   s.push(ind);
+        order.push(ind);
+        return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // unordered_map<int, vector<int>> adj;
-        vector<int> adj[numCourses];
-        
+        unordered_map<int, vector<int>> mp;
         for(auto i:prerequisites){
-            adj[i[1]].push_back(i[0]);
+            mp[i[1]].push_back(i[0]);
         }
-        
-        vector<int> in(numCourses, 0);
-        for(auto i:prerequisites){
-            in[i[0]]++;
-        }
-        
-        queue<int> q;
+        stack<int> order;
+        vector<bool> vis(numCourses, false);
+        vector<bool> dfsVis(numCourses, false);
         for(int i=0;i<numCourses;i++){
-            if(in[i] == 0)  q.push(i);
-        }
-        int count = 0;
-        vector<int> ans;
-        while(!q.empty()){
-            auto curr = q.front();
-            q.pop();
-            ans.push_back(curr);
-            count++;
-            for(auto i:adj[curr]){
-                in[i]--;
-                if(in[i] == 0)  q.push(i);
+            if(!vis[i]){
+                if(dfs(numCourses, mp, i, order, vis, dfsVis))  return {};
             }
         }
-        if(count == numCourses) return ans;
-        return {};
-        // vector<bool> vis(numCourses, false);
-        // vector<bool> dfsVis(numCourses, false);
-        // stack<int> s;
-        // vector<int> ans;
-//         bool possible = false;
-//         for(int i=0;i<numCourses;i++){
-//             dfs(adj, 0, vis, dfsVis, s, possible);
-//         }
-        
-//         if(possible)  return {};
-//         while(!s.empty()){
-//             ans.push_back(s.top());
-//             s.pop();
-//         }
-//         return ans;
-        
+        vector<int> ans;
+        while(!order.empty()){
+            ans.push_back(order.top());
+            order.pop();
+        }
+        return ans;
     }
 };
