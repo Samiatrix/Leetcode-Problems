3190a834cd6162195c0668630a9ad1f78002ece5
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-07 15:17:19 +0530

    Time: 83 ms (47.00%), Space: 27.5 MB (25.31%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool mat(string& s, int i, string& p, int j, vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
-        if(j<0 && i>=0) return false;
-        if(i<0 && j>=0){
+    bool match(string& s, int i, string& p, int j, vector<vector<int>>& dp){
+        if(j < 0 && i < 0)  return true;
+        if(j < 0 && i >= 0)    return false;
+        if(i < 0 && j >= 0){
             for(int k=0;k<=j;k++){
                 if(p[k]!='*')   return false;
             }
             return true;
         }
         if(dp[i][j]!=-1)    return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?') return dp[i][j] = mat(s, i-1, p, j-1, dp);
-        if(p[j] == '*') return dp[i][j] = mat(s, i-1, p, j, dp) || mat(s, i, p, j-1, dp);
+        if(s[i] == p[j])    return dp[i][j] = match(s, i-1, p, j-1, dp);
+        if(p[j] == '?')    return dp[i][j] = match(s, i-1, p, j-1, dp);
+        if(p[j] == '*')    return dp[i][j] = match(s, i, p, j-1, dp) || match(s, i-1, p, j, dp);
         return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
         vector<vector<int>> dp(s.size(), vector<int>(p.size(), -1));
-        return mat(s, s.size()-1, p, p.size()-1, dp);
+        return match(s, s.size()-1, p, p.size()-1, dp);
     }
 };
