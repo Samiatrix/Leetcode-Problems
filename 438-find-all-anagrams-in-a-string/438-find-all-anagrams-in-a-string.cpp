3ca86543338106538f78d7c3fbc62b280f3c2313
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-11 01:01:28 +0530

    Time: 24 ms (43.60%), Space: 8.7 MB (39.11%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         unordered_map<char, int> mp;
-        for(auto i:p)   mp[i]++;
+        for(auto i:p){
+            mp[i]++;
+        }
         
-        vector<int> ans;
         int freq = 0;
-        for(int i = 0, j = 0;j<s.size();j++){
+        vector<int> ans;
+        
+        for(int i=0, j=0;j<s.size();j++){
             if(mp.count(s[j])){
                 mp[s[j]]--;
                 if(mp[s[j]] == 0)   freq++;
             }
-            if(freq == mp.size())   ans.push_back(i);
             
+            if(freq == mp.size())   ans.push_back(i);
             if(j>=p.size()-1){
                 char ch = s[i++];
                 if(mp.count(ch)){
