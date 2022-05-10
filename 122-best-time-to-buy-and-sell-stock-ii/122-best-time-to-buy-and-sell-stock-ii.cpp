b744b771b910ae9969ecc36024014847df10e421
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-10 15:45:53 +0530

    Time: 31 ms (5.27%), Space: 15.2 MB (13.13%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -11,7 +11,18 @@ public:
         }
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
-        return profit(0, 1, prices, dp);
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, 0));
+        dp[prices.size()][0] = dp[prices.size()][1] = 0;
+        for(int ind = prices.size()-1;ind>=0;ind--){
+            for(int canBuy = 0;canBuy<=1;canBuy++){
+                if(canBuy){
+                    dp[ind][canBuy] = max(-prices[ind] + dp[ind+1][0], dp[ind+1][1]);
+                }
+                else{
+                    dp[ind][canBuy] = max(prices[ind] + dp[ind+1][1], dp[ind+1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
