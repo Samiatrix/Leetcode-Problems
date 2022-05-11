69f88367b010c152cded470ae886952c9bd82554
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-11 13:45:36 +0530

    Time: 120 ms (12.22%), Space: 82.1 MB (5.26%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int dis(string s, int i, string t, int j, vector<vector<int>>& dp){
-        if(i==0) return j;
-        if(j==0) return i;
-        // if(i == 0 && j == 0)    return 0;
+    int dist(string s, int i, string t, int j, vector<vector<int>>& dp){
+        if(i == 0)  return j;
+        if(j == 0)  return i;
         if(dp[i][j]!=-1)    return dp[i][j];
-        if(s[i-1] == t[j-1])    return dp[i][j] = dis(s, i-1, t, j-1, dp);
+        if(s[i-1] == t[j-1])    return dp[i][j] = dist(s, i-1, t, j-1, dp);
         else{
-            return dp[i][j] = 1+min({dis(s, i-1, t, j, dp), dis(s, i, t, j-1, dp), dis(s, i-1, t, j-1, dp)});
+            return dp[i][j] = 1+min({dist(s, i-1, t, j, dp), dist(s, i, t, j-1, dp), dist(s, i-1, t, j-1, dp)});
         }
     }
     int minDistance(string word1, string word2) {
-        if(word1 == "" || word2 == "")  return max(word1.size(), word2.size());
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        return dis(word1, word1.size(), word2, word2.size(), dp);
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return dist(word1, n, word2, m, dp);
     }
 };
