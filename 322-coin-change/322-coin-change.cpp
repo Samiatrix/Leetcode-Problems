9d7a582115aeeac55741ab8b1b5bec898a1f0868
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-23 10:37:55 +0530

    Time: 109 ms (60.04%), Space: 45.2 MB (18.63%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -14,6 +14,16 @@ public:
         sort(coins.begin(), coins.end());
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        // vector<int> t(amount+1, 0);
+        // dp[0] = t;
+        // for(int i = 1;i<=amount;i++){
+        //     for(int j=0;j<n;j++){
+        //         int take = INT_MAX;
+        //         if(coins[j]<=i) take = dp[j][i-coins[j]];
+        //         int notTake = dp[j-1][i];
+        //     }
+        // }
+        // int ans = dp[coins.size()-1][amount];
         int ans = coin(coins, coins.size()-1, amount, dp);
         return ans == INT_MAX ? -1 : ans;
     }
