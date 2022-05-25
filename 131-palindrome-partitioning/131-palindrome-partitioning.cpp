c6b6dd78874d088bd31fd9cbe7b8a0853efed650
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-25 06:18:39 +0530

    Time: 344 ms (15.66%), Space: 144.9 MB (17.70%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -6,7 +6,7 @@ public:
         }
         return true;
     }
-    void dfs(string s, int ind, vector<string> curr, vector<vector<string>>& ans){
+    void pal(string s, int ind, vector<string> curr, vector<vector<string>>& ans){
         if(ind>=s.size()){
             ans.push_back(curr);
             return;
@@ -14,15 +14,15 @@ public:
         for(int i=ind;i<s.size();i++){
             if(isPalindrome(s, ind, i)){
                 curr.push_back(s.substr(ind, i-ind+1));
-                dfs(s, i+1, curr, ans);
+                pal(s, i+1, curr, ans);
                 curr.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
         vector<string> curr;
-        dfs(s, 0, curr, ans);
+        vector<vector<string>> ans;
+        pal(s, 0, curr, ans);
         return ans;
     }
 };
