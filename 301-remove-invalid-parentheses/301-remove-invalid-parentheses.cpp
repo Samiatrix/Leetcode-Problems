858dcf28ff5699eddb8eacfbf8110146d247626a
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-26 02:29:08 +0530

    Time: 128 ms (56.69%), Space: 32.5 MB (35.74%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool isValid(string s){
+    bool isValid(string curr){
         int count = 0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == '(') count++;
-            else if(s[i] == ')'){
+        for(auto i:curr){
+            if(i == '(')    count++;
+            else if(i == ')'){
                 if(count>0) count--;
                 else    return false;
             }
         }
-        return !count;
+        return count == 0;
     }
     vector<string> removeInvalidParentheses(string s) {
         queue<string> q;
         q.push(s);
-        vector<string> ans;
         unordered_set<string> st;
+        vector<string> ans;
         while(!q.empty()){
             auto curr = q.front();
             q.pop();
@@ -24,7 +24,7 @@ public:
             if(isValid(curr))   ans.push_back(curr);
             else if(ans.empty()){
                 for(int i=0;i<curr.size();i++){
-                    if(curr[i] == '(' || curr[i] == ')')  q.push(curr.substr(0, i) + curr.substr(i+1));
+                    if(curr[i] == '(' || curr[i] == ')')    q.push(curr.substr(0, i)+curr.substr(i+1));
                 }
             }
         }
