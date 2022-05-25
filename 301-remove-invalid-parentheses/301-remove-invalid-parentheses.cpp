6e170f651b884a9baaeaa23d9975cf7261b3e3e9
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-26 03:05:41 +0530

    Time: 31 ms (79.45%), Space: 13.4 MB (67.14%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool isValid(string curr){
+    bool isValid(string s){
         int count = 0;
-        for(auto i:curr){
+        for(auto i:s){
             if(i == '(')    count++;
             else if(i == ')'){
                 if(count>0) count--;
@@ -14,20 +14,20 @@ public:
     vector<string> removeInvalidParentheses(string s) {
         queue<pair<string, int>> q;
         q.push({s, 0});
-        // unordered_set<string> st;
         vector<string> ans;
         while(!q.empty()){
-            auto curr = q.front();
+            auto p = q.front();
             q.pop();
-            // if(st.count(curr))  continue;
-            // st.insert(curr);
-            string st = curr.first;
-            int ind = curr.second;
-            
-            if(isValid(st))   ans.push_back(st);
+            string curr = p.first;
+            int ind = p.second;
+            if(isValid(curr)){
+                ans.push_back(curr);
+            }
             else if(ans.empty()){
-                for(int i=ind;i<st.size();i++){
-                    if((st[i] == '(' || st[i] == ')') && (i==ind || st[i-1] != st[i]))    q.push({st.substr(0, i)+st.substr(i+1), i});
+                for(int i=ind;i<curr.size();i++){
+                    if((curr[i] == '(' || curr[i] == ')') && (i == ind || curr[i]!=curr[i-1])){
+                        q.push({curr.substr(0, i) + curr.substr(i+1), i});
+                    }
                 }
             }
         }
