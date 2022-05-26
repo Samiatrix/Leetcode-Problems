d87dbe073d823c59cff47519dbebb91ad60b6e40
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-26 23:56:00 +0530

    Time: 25 ms (42.08%), Space: 16 MB (56.15%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -14,12 +14,13 @@ public:
     vector<TreeNode*> gen(int l, int r){
         if(l>r) return {NULL};
         vector<TreeNode*> ans;
-        for(int i=l;i<=r;i++){
+        
+        for(int i=l;i<=r;i++){  
             vector<TreeNode*> le = gen(l, i-1);
             vector<TreeNode*> ri = gen(i+1, r);
             
             for(int j=0;j<le.size();j++){
-                for(int k = 0;k<ri.size();k++){
+                for(int k=0;k<ri.size();k++){
                     TreeNode* curr = new TreeNode(i);
                     curr->left = le[j];
                     curr->right = ri[k];
@@ -30,7 +31,7 @@ public:
         return ans;
     }
     vector<TreeNode*> generateTrees(int n) {
-        if(n == 0)  return {};
+        if(!n)  return {};
         return gen(1, n);
     }
 };
