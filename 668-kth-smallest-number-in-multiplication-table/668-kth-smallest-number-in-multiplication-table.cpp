c4be927a876a9d1baa7857332bceafe898b7fdf7
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-26 23:28:11 +0530

    Time: 14 ms (84.36%), Space: 5.9 MB (73.71%) - LeetHub

--- 668-kth-smallest-number-in-multiplication-table/668-kth-smallest-number-in-multiplication-table.cpp
+++ 668-kth-smallest-number-in-multiplication-table/668-kth-smallest-number-in-multiplication-table.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countLessThanOrEqualToMid(int& n, int& m, int mid){
+    int countLessThanOrEqualToMid(int n, int m, int mid){
         int count = 0;
         for(int i=1;i<=n;i++){
             count += min((mid/i), m);
@@ -12,7 +12,7 @@ public:
         int ans = -1;
         while(l<=r){
             int mid = l+(r-l)/2;
-            if(countLessThanOrEqualToMid(m, n, mid)>=k){
+            if(countLessThanOrEqualToMid(m, n, mid) >= k){
                 ans = mid;
                 r = mid-1;
             }
