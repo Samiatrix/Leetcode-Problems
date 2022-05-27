b54a86899051abe2bd61b6b3bffa4913589a1c89
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-27 10:30:13 +0530

    Time: 152 ms (39.49%), Space: 20.1 MB (67.93%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,21 +5,18 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size();i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-            int t = -nums[i];
             int l = i+1, r = nums.size()-1;
+            int tar = -nums[i];
             while(l<r){
-                int sum = nums[l]+nums[r];
-                if(sum == t){
+                if(nums[l]+nums[r] == tar){
                     ans.push_back({nums[i], nums[l], nums[r]});
-                    while(l<r && nums[l+1] == nums[l]) l++;
-                    while(l<r && nums[r-1] == nums[r]) r--;
-                }
-                if(sum < t){
+                    while(l<r && nums[l] == nums[l+1])  l++;
+                    while(l<r && nums[r] == nums[r-1])  r--;
                     l++;
-                }
-                else{
                     r--;
                 }
+                else if(nums[l] + nums[r] < tar)    l++;
+                else    r--;
             }
         }
         return ans;
