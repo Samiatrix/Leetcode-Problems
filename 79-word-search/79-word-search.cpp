1249e8fb5fe3b48e03b93114663c5459ca742f1e
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-05-27 12:15:24 +0530

    Time: 688 ms (39.16%), Space: 7.9 MB (91.48%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& grid, int i, int j, string s, int ind){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!=s[ind])  return false;
-        if(ind == s.size()-1)   return true;
-        char ch = grid[i][j];
-        grid[i][j] = '*';
-        if(check(grid, i+1, j, s, ind+1))   return true;
-        if(check(grid, i-1, j, s, ind+1))   return true;
-        if(check(grid, i, j+1, s, ind+1))   return true;
-        if(check(grid, i, j-1, s, ind+1))   return true;
-        grid[i][j] = ch;
-        return false;
+    bool find(vector<vector<char>>& mat, int i, int j, string word, int ind){
+        if(i<0 || i>=mat.size() || j<0 || j>=mat[0].size()) return false;
+        if(word[ind]!=mat[i][j])    return false;
+        if(ind == word.size()-1)  return true;
+        char ch = mat[i][j];
+        mat[i][j] = '*';
+        bool f = find(mat, i+1, j, word, ind+1) || find(mat, i-1, j, word, ind+1) || find(mat, i, j+1, word, ind+1) || find(mat, i, j-1, word, ind+1);
+        mat[i][j] = ch;
+        return f;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[i].size();j++){
                 if(board[i][j] == word[0]){
-                    if(check(board, i, j, word, 0)) return true;
+                    if(find(board, i, j, word, 0))  return true;
                 }
             }
         }
