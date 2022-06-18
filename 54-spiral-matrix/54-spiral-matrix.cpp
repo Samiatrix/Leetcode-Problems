6205e56f44f4729f8614e3d2c6a7d115778de537
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-18 13:09:14 +0530

    Time: 3 ms (42.17%), Space: 6.8 MB (91.53%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int rs = 0, re = matrix.size()-1;
-        int cs = 0, ce = matrix[0].size()-1;
-        int dir = 0;
         int n = matrix.size(), m = matrix[0].size();
-        while(n*m != ans.size()){
-            if(dir == 0){
-                for(int i=cs;i<=ce;i++){
-                    ans.push_back(matrix[rs][i]);
-                }
-                rs++;
-                dir = 1;
+        int rs = 0, re = n-1;
+        int cs = 0, ce = m-1;
+        vector<int> ans;
+        while(ans.size() != m*n){
+            for(int i=cs;i<=ce;i++){
+                ans.push_back(matrix[rs][i]);
             }
-            else if(dir == 1){
-                for(int i=rs;i<=re;i++){
-                    ans.push_back(matrix[i][ce]);
-                }
-                ce--;
-                dir = 2;
+            if(ans.size() == m*n)   return ans;
+            rs++;
+            for(int i=rs;i<=re;i++){
+                ans.push_back(matrix[i][ce]);
             }
-            else if(dir == 2){
-                for(int i=ce;i>=cs;i--){
-                    ans.push_back(matrix[re][i]);
-                }
-                re--;
-                dir = 3;
+            if(ans.size() == m*n)   return ans;
+            ce--;
+            for(int i=ce;i>=cs;i--){
+                ans.push_back(matrix[re][i]);
             }
-            else if(dir == 3){
-                for(int i=re;i>=rs;i--){
-                    ans.push_back(matrix[i][cs]);
-                }
-                cs++;
-                dir = 0;
+            if(ans.size() == m*n)   return ans;
+            re--;
+            for(int i=re;i>=rs;i--){
+                ans.push_back(matrix[i][cs]);
             }
+            if(ans.size() == m*n)   return ans;
+            cs++;
         }
         return ans;
     }
