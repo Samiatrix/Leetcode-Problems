54d48228c9aa96de89cd436bc5eabf6816ed670c
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-21 16:54:51 +0530

    Time: 3 ms (97.85%), Space: 8.9 MB (34.78%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,7 +10,17 @@ public:
         return dp[i][j] = min(bot, dia);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.back().size(), -1));
-        return sum(triangle, 0, 0, dp);
+        // vector<vector<int>> dp(triangle.size(), vector<int>(triangle.back().size(), -1));
+        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.back().size(), 0));
+        dp.back() = triangle.back();
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int bot = triangle[i][j] + dp[i+1][j];
+                int dia = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(bot, dia);
+            }
+        }
+        return dp[0][0];
+        // return sum(triangle, 0, 0, dp);
     }
 };
