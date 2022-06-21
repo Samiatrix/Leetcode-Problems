311931a6fccc8fc3a7ab19ed973529360de4281a
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-21 23:29:00 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.88%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int step = 0;
+        int sum = 0;
+        while(sum<target){
+            step++;
+            sum+=step;
+        }
+        while((sum-target)%2 != 0){
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+};
