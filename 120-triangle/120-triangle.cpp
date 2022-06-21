0e641b83bd0f1a4b9698f9f49cbcb57f42e7e97f
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-21 16:50:32 +0530

    Time: 18 ms (8.61%), Space: 9.1 MB (7.76%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int total(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(i == triangle.size()-1)    return triangle[i][j];
+    int sum(vector<vector<int>>& mat, int i, int j, vector<vector<int>>& dp){
+        if(i == mat.size()-1){
+            return mat[i][j];
+        }
         if(dp[i][j]!=-1)    return dp[i][j];
-        int bel = triangle[i][j] + total(i+1, j, triangle, dp);
-        int dia = triangle[i][j] + total(i+1, j+1, triangle, dp);
-        return dp[i][j] = min(bel, dia);
+        int bot = mat[i][j] + sum(mat, i+1, j, dp);
+        int dia = mat[i][j] + sum(mat, i+1, j+1, dp);
+        return dp[i][j] = min(bot, dia);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         vector<vector<int>> dp(triangle.size(), vector<int>(triangle.back().size(), -1));
-        return total(0, 0, triangle, dp);
+        return sum(triangle, 0, 0, dp);
     }
 };
