ca8afd0699b873a1e710809babf27d09dd5ea0cd
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-23 01:32:06 +0530

    Time: 12 ms (12.80%), Space: 7.8 MB (12.17%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    void restore(string& s, int ind, int level, string curr, vector<string>& ans){
-        if(level == 4 && ind>=s.size()){
+    void restore(string s, int ind, int level, string curr, vector<string>& ans){
+        if(ind >= s.size() && level == 4){
             ans.push_back(curr.substr(0, curr.size()-1));
+            return ;
         }
+        
         int n = 0;
         for(int i=ind;i<ind+3 && i<s.size();i++){
-            n = n*10 + (s[i]-'0');
-            if(n <= 255){
-                restore(s, i+1, level+1, curr+to_string(n)+".", ans);
+            n = n * 10 + (s[i]-'0');
+            if(n<=255){
+                restore(s, i+1, level + 1, curr + to_string(n) + ".", ans);
             }
-            if(n == 0)  break;
+            if(n == 0){
+                break;
+            }
         }
     }
     vector<string> restoreIpAddresses(string s) {
