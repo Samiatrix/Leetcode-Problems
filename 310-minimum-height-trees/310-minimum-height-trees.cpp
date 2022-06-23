38383ac058fb94d96b133e32b8bccd7e1492affc
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-23 19:14:45 +0530

    Time: 273 ms (43.75%), Space: 64 MB (29.19%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    void print(unordered_map<int, vector<int>>& mp){
-        for(auto i:mp){
-            cout<<i.first<<" : ";
-            for(auto j:i.second){
-                cout<<j<<" ";
-            }
-            cout<<endl;
-        }
-    }
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if(n == 1)  return {0};
         unordered_map<int, vector<int>> mp;
@@ -16,8 +7,6 @@ public:
             mp[i[0]].push_back(i[1]);
             mp[i[1]].push_back(i[0]);
         }
-        
-        print(mp);
         
         queue<int> q;
         
