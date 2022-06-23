cb870e59e439b465c41c8da49f92973b47d14846
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-24 01:48:38 +0530

    Time: 428 ms (27.07%), Space: 141.2 MB (6.46%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    bool part(int n, int k, vector<int>& arr){
-        vector<bool> dp(k+1, false), t(k+1, false);
-        dp[0] = true;
-        t[0] = true;
-        if(arr[0]<=k)    dp[arr[0]] = true;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=k;j++){
-                bool take = (j>=arr[i] ? dp[j-arr[i]] : false);
-                bool notTake = dp[j];
-                t[j] = take | notTake;
-            }
-            dp = t;
-        }
-        return dp[k];
+    bool sub(vector<int>& nums, int ind, int tar, vector<vector<int>>& dp){
+        if(ind>=nums.size())    return false;
+        if(nums[ind] == tar)  return true;
+        if(dp[ind][tar]!=-1)    return dp[ind][tar];
+        bool notTake = sub(nums, ind-1, tar, dp);
+        bool take = false;
+        if(tar>=nums[ind])  take = sub(nums, ind-1, tar-nums[ind], dp);
+        return dp[ind][tar] = take || notTake;
     }
     bool canPartition(vector<int>& nums) {
-        if(nums.size() == 1)    return false;
         int sum = 0;
-        for(auto i:nums)    sum+=i;
-        if(sum&1)   return false;
-        sum/=2;
-        return part(nums.size()-1, sum, nums);
+        for(auto i:nums){
+            sum+=i;
+        }
+        if(sum & 1) return false;
+        int tar = sum/2;
+        // vector<vector<int>> dp(nums.size()+1, vector<int>(tar+1, -1));
+        vector<vector<int>> dp(nums.size()+1, vector<int>(sum+1, false));
+        // return sub(nums, 0, tar, dp);
+        for(int i=0;i<nums.size();i++){
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for(int i=1;i<nums.size();i++){
+            for(int j = 1;j<=tar;j++){
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if(j>=nums[i])  take = dp[i-1][j-nums[i]];
+                dp[i][j] = take | notTake;
+            }
+        }
+        return dp[nums.size()-1][tar];
     }
 };
