80e86b803498f1ade89ad31fbb1d4d5ab1096e9c
Haridas <37403386+Samiatrix@users.noreply.github.com>
2022-06-24 15:10:04 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (46.44%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -9,7 +9,15 @@ public:
         return dp[ind] = max(take, notTake);
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return rob(nums, nums.size()-1, dp);
+        // vector<int> dp(nums.size(), -1); //for recursion+memo
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            int notTake = dp[i-1];
+            int take = nums[i];
+            if(i>1)   take += dp[i-2];
+            dp[i] = max(take, notTake);
+        }
+        return dp[nums.size()-1];
     }
 };
